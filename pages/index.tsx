75782cd7c6341b1e634b7a1b55b4e570b7326aa5
Kumar Ritu Raj <46402785+Kumar-Ritu-Raj@users.noreply.github.com>
2023-11-05 15:50:46 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,9 @@ export default function Home() {
   const [reamount, setReamount] = useState<number>(initialReamount);
 
   const CalculateAmount = () => {
-    const parsedAmount = parseInt(amount, 10);
-    const parsedRate = parseInt(rate, 10);
-    const parsedReamount = parseInt(reamount, 10);
+    const parsedAmount = parseInt(amount.toString(), 10);
+    const parsedRate = parseInt(rate.toString(), 10);
+    const parsedReamount = parseInt(reamount.toString(), 10);
 
     if (parsedAmount <= 0 || parsedRate <= 0 || parsedReamount <= 0) {
       alert('Please enter valid positive values for all inputs./कृपया संख्या भरें');
@@ -51,15 +51,15 @@ export default function Home() {
         <ul>
           <li className='bg-white my-[15px] ml-[20px]'>
             <p>Enter Total Amount/कितने रुपये देने हैं:-</p>
-            <div><input className='border-[#123] border-[1px] rounded-sm' type="number" value={amount} onChange={e => setAmount(e.target.value)} min="0" /></div>
+            <div><input className='border-[#123] border-[1px] rounded-sm' type="number" value={amount} onChange={e => setAmount(parseFloat(e.target.value))} min="0" /></div>
           </li>
           <li className='bg-white my-[15px] ml-[20px]'>
             <p>Enter Rate%/कितने दर पर:-</p>
-            <div><input className='border-[#123] border-[1px]' type="number" value={rate} onChange={e => setRate(e.target.value)} min="0" /></div>
+            <div><input className='border-[#123] border-[1px]' type="number" value={rate} onChange={e => setRate(parseFloat(e.target.value))} min="0" /></div>
           </li>
           <li className='bg-white my-[15px] ml-[20px]'>
             <p>Enter Return Amount/प्रतिमाह कितने रुपये लेने हैं:-</p> 
-            <div><input className='border-[#123] border-[1px]' type="number" value={reamount} onChange={e => setReamount(e.target.value)} min="0" /></div>
+            <div><input className='border-[#123] border-[1px]' type="number" value={reamount} onChange={e => setReamount(parseFloat(e.target.value))} min="0" /></div>
           </li>
           <li className='text-center mt-[20px]'>
             <button className='bg-indigo-700 rounded-lg p-2 text-[#fff] text-[20px] mx-auto text-center' onClick={CalculateAmount}>Calculate</button>
